7771d708cfeb2acd229e19f86d2d2cdf5d7f6dda
Connor Stauffer <connor.paul.stauffer@gmail.com>
2015-07-02 10:54:03 -0700

    create basic piece attributes

--- piece.rb
+++ piece.rb
@@ -1,4 +1,34 @@
 class Piece
 
+  attr_reader :color, :pos
+
+  def initialize(color, pos)
+    @color = color
+    @pos = pos
+  end
+
+  def empty?
+    false
+  end
+
+  def piece?
+    true
+  end
+
+  def to_s
+    (color == :red) ? "\u25C9".red : "\u25C9".black
+  end
+
+end
+
+class EmptySquare
+
+  def empty?
+    true
+  end
+
+  def piece?
+    false
+  end
 
 end
